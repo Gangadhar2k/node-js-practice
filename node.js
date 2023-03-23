5b0fc43782d084cc4f0ae7d711417b64b88f9b96
Gangadhar <gangadhar2k.official@gmail.com>
2023-03-23 20:07:40 +0530

    practicing how to  redirect the page

--- node.js
+++ node.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
 const reListener = (req,res) =>{
-
+  const method = req.method;
   const url = req.url;
+
+  //  if the url === / 
   if (url === '/') {
     res.setHeader('content-type','text/html');
     res.write('<html>');
@@ -11,7 +13,27 @@ const reListener = (req,res) =>{
     res.write('</html>');
     return res.end(); 
   }
+
+   
+  // redirecting code 
+
+  if(url === '/message' && method == 'POST' ){
+    res.setHeader('Location','/test');
+    res.statusCode = 302;
+    return res.end();
+  }
   
+  
+  if (url === '/test') {
+    res.setHeader('content-type','text/html');
+    res.write('<html>');
+    res.write('<head><title>first</title></head>');
+    res.write('<body><h1> Redirected Web Page</h1><br></body>');
+    res.write('</html>');
+    return res.end(); 
+  }
+  
+
   res.setHeader('content-type','text/html');
   res.write('<html>');
   res.write('<head><title>first</title></head>');
